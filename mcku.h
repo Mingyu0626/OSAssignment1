bab0fc37033869d7d5fc80292ffa7913dfc47c86
Mingyu0626 <mg010626@naver.com>
2023-05-08 17:21:40 +0900

    Update mcku.h

--- mcku.h
+++ mcku.h
@@ -1,92 +1,74 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <string.h>
 #include <stdbool.h>
 
-struct pcb{
-	char pid;
-	FILE *fd;
-	char *pgtable;
-	/* Add more fields if needed */
-	bool isExit;
-};
-
-// to use from mcku.c
-extern struct pcb* current;
-extern char* ptbr;
-//PCB Struct Arrays
-struct pcb* pcbs;
-
-// Num of Processes
-int processLength;
-
 char* deleteNewLine(char* str);
 
-// pid를 1씩 올려서 current에 넣어주는 스케줄링
+struct pcb {
+    unsigned char pid;
+    FILE* fd;
+    char* pgtable;
+    bool isExit;
+};
+
+struct pcb* pcbs;
+int processLength;
+extern struct pcb* current;
+extern char* ptbr;
+
 void ku_scheduler(char pid) {
-	int count = 0;
+    int count = 0;
 
-	do {
-		current = &pcbs[++pid % processLength];
-	} while (current->isExit && count++ < processLength);
+    do {
+        current = &pcbs[++pid % processLength];
+        ptbr = current->pgtable;
+    } while (current->isExit && count++ < processLength);
 
-	if (count == processLength) {
-		exit(0);
-	}
+    if (count >= processLength) {
+        exit(0);
+    }
+}
+
+void ku_pgfault_handler(char va) {
+    int pt_index = (va & 0xF0) >> 4;
+    ptbr[pt_index] = 1;
 }
 
 
-void ku_pgfault_handler(char pid) {
-	// page fault가 발생한 virtual address를 인자로 받는다.
-	// 변수명은 pid이지만, 사실상 virtual address이다.
-	// 하지만 실제 어드레스를 다루는 것은 아니고 
-	// 어드레스를 흉내내는 값을 다루고 있는 것이다.
-	/* Your code here */
-	int pageTableIndex = (pid & 0xF0) >> 4;
-	ptbr[pageTableIndex] = 1;
+void ku_proc_exit(char pid) {
+    pcbs[pid].isExit = true;
 }
 
 
-void ku_proc_exit(char pid){
-	/* Your code here */
-	pcbs[pid].isExit = true;
-}
+void ku_proc_init(int nprocs, char* flist) {
+    processLength = nprocs;
+    FILE* fileList = fopen(flist, "r");
+    pcbs = malloc(sizeof * pcbs * nprocs);
+    size_t len = 0;
 
+    for (int i = 0; i < nprocs; i++) {
+        char* processFileName = NULL;
+        getline(&processFileName, &len, fileList);
+        processFileName = deleteNewLine(processFileName);
 
-void ku_proc_init(int nprocs, char *flist){
-	/* Your code here */
-	processLength = nprocs;
-	FILE* fl = fopen(flist, "r");
-	pcbs = malloc(sizeof * pcbs * nprocs);
-	size_t len = 0;
+        pcbs[i].fd = fopen(processFileName, "r");
+        pcbs[i].pid = i;
+        pcbs[i].pgtable = malloc(sizeof * pcbs->pgtable * 16);
+        pcbs[i].isExit = false;
 
-
-	for (int i = 0; i < nprocs; i++) {
-		char* processFileName = NULL;
-		getline(&processFileName, &len, fl);
-		if (i == nprocs - 1) 
-			processFileName = deleteNewLine(processFileName);
-
-
-		pcbs[i].fd = fopen(processFileName, "r");
-		pcbs[i].pid = i;
-		pcbs[i].pgtable = malloc(sizeof * pcbs->pgtable * 16);
-		// pcbs[i].pgtable = 0x00;
-		pcbs[i].isExit = false;
-		free(processFileName);
-	}
-	current = &pcbs[0];
-	ptbr = current->pgtable;
-
+        free(processFileName);
+    }
+    current = &pcbs[0];
+    ptbr = current->pgtable;
 }
 
 char* deleteNewLine(char* str) {
-	if (str[strlen(str) - 1] != '\n') 
-		return str;
+    if (str[strlen(str) - 1] != '\n') 
+        return str;
 
-	char* newStr = (char*)malloc(strlen(str) - 1);
+    char* newStr = (char*)malloc(strlen(str) - 1);
 
-	for (int i = 0; i < strlen(str) - 1; i++) 
-		newStr[i] = str[i];
+    for (int i = 0; i < strlen(str) - 1; i++) 
+        newStr[i] = str[i];
 
-	return newStr;
+    return newStr;
 }
