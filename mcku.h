e2023f61d816ff1b99f41db516c33fe2ebff38bd
Mingyu0626 <mg010626@naver.com>
2023-05-10 17:17:51 +0900

    Update mcku.h

--- mcku.h
+++ mcku.h
@@ -60,7 +60,7 @@ void ku_proc_exit(char pid) {
     }
     free(pcbs[pid].pgtable);
     fclose(pcbs[pid].fd);
-    printf("proc_exit 호출 : %d번 프로세스 종료\n", pid);
+    printf("proc_exit called : process %d is exited \n", pid);
 }
 
 
